8a055a506157b8a7f8d5da92e9a0c93889fa203d
mds <17163988+mds1@users.noreply.github.com>
2019-04-03 20:42:15 -0400

    Add blockchain projects

--- src/components/blockchainProjectsList.js
+++ src/components/blockchainProjectsList.js
@@ -16,7 +16,7 @@ const items = [
     tools: [
       { name: 'Vue', url: 'https://vuejs.org/' },
       { name: 'Quasar Framework', url: 'https://quasar-framework.org' },
-      { name: 'MySportsFeeds', url: 'https://www.mysportsfeeds.com/' },
+      { name: 'MySportsFeeds API', url: 'https://www.mysportsfeeds.com/' },
       { name: 'Truffle Suite', url: 'https://truffleframework.com/' },
       { name: 'Ganache CLI', url: 'https://github.com/trufflesuite/ganache-cli' },
       { name: 'Infura', url: 'https://infura.io/' },
@@ -26,17 +26,41 @@ const items = [
     ],
     urlGit: 'https://github.com/mds1/survivor-frontend',
     urlHome: 'https://survivor-frontend.netlify.com/',
+    imgPath: 'statics/projects/blockchain/survivor.png',
     showIf: true,
   },
-  // {
-  //   name: 'NFL Survivor',
-  //   description: 'blah blah',
-  //   tools: [{}],
-  //   urlGit: 'https://github.com/mds1/survivor-frontend',
-  //   urlHome: 'https://survivor-frontend.netlify.com/',
-  //   imgPath: 'statics/projects/blockchain/survivor.png',
-  //   showIf: true,
-  // },
+  {
+    name: 'Send ETH Transactions',
+    description: `Simple webapp to send Ethereum transactions using MetaMask,
+      with Ethereum Name Service (ENS) support.`,
+    tools: [
+      { name: 'Vue', url: 'https://vuejs.org/' },
+      { name: 'Quasar Framework', url: 'https://quasar-framework.org' },
+      { name: 'Infura', url: 'https://infura.io/' },
+      { name: 'Ethereum Name Service (ENS)', url: 'https://ens.domains' },
+    ],
+    urlGit: 'https://github.com/mds1/send-eth-tx',
+    urlHome: 'https://sendethtx.netlify.com/',
+    imgPath: 'statics/projects/blockchain/sendtx.png',
+    showIf: true,
+  },
+  {
+    name: 'KeyStats',
+    description: `Enter an Ethereum address and get various figures, tables,
+      and plots summarizing transactions of that address.
+      <i>
+        This site is currently broken, as CoinMarketCap has since changed
+        their API.
+      </i>`,
+    tools: [
+      { name: 'Etherscan API', url: 'https://etherscan.io' },
+      { name: 'CoinMarketCap API', url: 'https://coinmarketcap.com' },
+    ],
+    urlGit: 'https://github.com/mds1/KeyStats',
+    urlHome: 'https://keystats.netlify.com/',
+    imgPath: 'statics/projects/blockchain/keystats.png',
+    showIf: true,
+  },
 ];
 
 // For each item, add ID's based on the order they are in
